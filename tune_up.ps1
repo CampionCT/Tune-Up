a1331a16e9d2343d22faeca10550aba8a9c04432
CampionCT <ciarancampion4@gmail.com>
2025-02-23 15:43:49 -0800

    Adding echo to ps1 for visual notficiation

--- tune_up.ps1
+++ tune_up.ps1
@@ -3,11 +3,14 @@ Import-Module PSWindowsUpdate
 
 Get-WindowsUpdate -ListAvailable | Where-Object {$_.IsInstalled -eq $false } | Install-WindowsUpdate
 
+echo
 sfc.exe /scannow
 
+echo
 dism.exe /online /cleanup-image /scanhealth
 dism.exe /online /cleanup-image /restorehealth
 
+echo
 winget.exe upgrade --accept-package-agreements -r
 
 Pause
